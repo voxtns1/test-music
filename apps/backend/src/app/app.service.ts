acb90145d59f40d62363a05322974bc4d195ea69
julio Jorquera Bustillos <julio@kleeen.software>
2022-09-13 05:44:36 -0300

    feat(connect api): connect cloud by front

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,8 +1,57 @@
+import { Preference, ResultMusic } from '@music/core/type';
+
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to backend!' };
+  getData(): Preference[] {
+    return [
+      {
+        category: 'author',
+        children: [
+          {
+            id: 0,
+            name: 'julio'
+          },
+          {
+            id: 1,
+            name: 'pepe'
+          },
+          {
+            id: 2,
+            name: 'andres'
+          },
+        ],
+      },
+      {
+        category: 'generic',
+        children: [
+          {
+            id: 0,
+            name: 'rock'
+          },
+          {
+            id: 1,
+            name: 'indigo'
+          },
+          {
+            id: 2,
+            name: 'clasic'
+          },
+        ],
+      }
+    ];
+  }
+
+  getQuery(query: string): ResultMusic[] {
+    return [
+      {
+        id: 1,
+        name: 'Una espada sin igual',
+        category: ['rock', 'andy'],
+        author: 'Los autenticos decadentes',
+        cover: 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/love-song-mixtape-album-cover-template-design-250a66b33422287542e2690b437f881b_screen.jpg?ts=1635176340'
+      }
+    ];
   }
 }
--- apps/frontend/src/app/modules/home/home.service.ts
+++ apps/frontend/src/app/modules/home/home.service.ts
@@ -1,7 +1,9 @@
-import { BehaviorSubject, Observable, map, of, switchMap, take, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, map, of, take, tap } from 'rxjs';
+import { Preference, ResultMusic } from '@music/core/type';
 
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Preference } from '@music/core/type';
+import { environment } from 'apps/frontend/src/environments/environment';
 
 @Injectable({
     providedIn: 'root',
@@ -10,6 +12,10 @@ export class HomeService {
     private _allPreferences: BehaviorSubject<Preference[]> = new BehaviorSubject<Preference[]>([]);
     private _resultsMusic: BehaviorSubject<any> = new BehaviorSubject(null);
     private _query: BehaviorSubject<string> = new BehaviorSubject("");
+
+    constructor(private readonly http: HttpClient) {
+
+    }
 
     get allPreferences$(): Observable<Preference[]> {
         return this._allPreferences.asObservable();
@@ -24,60 +30,15 @@ export class HomeService {
     }
 
     getData(): Observable<Preference[]> {
-      const data = [
-        {
-          category: 'author',
-          children: [
-            {
-              id: 0,
-              name: 'julio'
-            },
-            {
-              id: 1,
-              name: 'pepe'
-            },
-            {
-              id: 2,
-              name: 'andres'
-            },
-          ],
-        },
-        {
-          category: 'generic',
-          children: [
-            {
-              id: 0,
-              name: 'rock'
-            },
-            {
-              id: 1,
-              name: 'indigo'
-            },
-            {
-              id: 2,
-              name: 'clasic'
-            },
-          ],
-        }
-      ];
-      this._allPreferences.next(data);
-      return of(data);
+      return this.http.get<Preference[]>(`${environment.endPoint}/data`).pipe(tap(res => this._allPreferences.next(res)));
     }
 
-    getPreferenceByQuery(query: string | null): Observable<any[]> {
-      if(query) {
+    getPreferenceByQuery(query: string | null): Observable<ResultMusic[]> {
+      if(!query) return of([]);
+
+      return this.http.get<ResultMusic[]>(`${environment.endPoint}/data/results`, { params: { query }}).pipe(tap(res => {
         this._query.next(query);
-      }
-      const data: any[] = [
-        {
-          id: 1,
-          name: 'Una espada sin igual',
-          category: ['rock', 'andy'],
-          author: 'Los autenticos decadentes',
-          cover: 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/love-song-mixtape-album-cover-template-design-250a66b33422287542e2690b437f881b_screen.jpg?ts=1635176340'
-        }
-      ];
-      this._resultsMusic.next(data);
-      return of(data);
+        this._resultsMusic.next(res)
+      }));
   }
 }
